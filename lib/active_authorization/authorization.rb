f32f212c23f642ed2e0c722c49487869eb16b6c2
Marek L <nospam.keram@gmail.com>
2017-07-15 12:28:39 +0100

    Replace `method_missing` implementation with `methods.detect(FALLBACK)`

--- lib/active_authorization/authorization.rb
+++ lib/active_authorization/authorization.rb
@@ -4,11 +4,7 @@ module ActiveAuthorization
   class Authorization
     DEFAULT_STATUS = false
     MOD_SEPARATOR = '::'
-    CHECK_METHOD_PREFIX = 'can_'
-    CHECK_METHOD_SUFFIX = '?'
-    CHECK_METHOD_REGEXP = Regexp.compile(
-      Regexp.quote(CHECK_METHOD_SUFFIX)
-    )
+    FALLBACK = -> { :default_status }
 
     def self.inherited(other)
       ActiveAuthorization.tree[other.name.split(MOD_SEPARATOR).slice(1...-1)]
@@ -16,21 +12,13 @@ module ActiveAuthorization
     end
     private_class_method :inherited
 
-    def self.check_method?(method_name)
-      method_name.match?(CHECK_METHOD_REGEXP)
-    end
-
-    def self.check_method_name(message_name)
-      message_name.dup.concat(CHECK_METHOD_SUFFIX)
-    end
-
     def initialize(seeker:, receiver:)
       @seeker = seeker
       @receiver = receiver
     end
 
     def authorized?(message_name)
-      send(self.class.check_method_name(message_name))
+      send(responding_method(message_name))
     end
 
     def authorize!(message_name)
@@ -59,18 +47,8 @@ module ActiveAuthorization
       self.class::DEFAULT_STATUS
     end
 
-    def respond_to_missing?(method_name, include_private)
-      {
-        true => true,
-        false => super
-      }[self.class.check_method?(method_name)]
-    end
-
-    def method_missing(method_name, *arguments, &block)
-      {
-        true => -> { default_status },
-        false => -> { super }
-      }[respond_to_missing?(method_name, false)].call
+    def responding_method(message_name)
+      methods.detect(FALLBACK) { |meth| meth.match?(message_name) }
     end
   end
 end
